e42d032af3902445862a4fb708cca4e6b660395d
Bhavani R <133200979+greueye@users.noreply.github.com>
2025-05-22 16:24:17 +0530

    Create TodoItem.js

---
+++ frontendsrc/context/TodoItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TodoItem = ({ todo, onDelete }) => {
+  return (
+    <div className="todo-item">
+      <div className="todo-text">{todo.text}</div>
+      <button 
+        onClick={() => onDelete(todo._id)} 
+        className="delete-btn"
+      >
+        Delete
+      </button>
+</div>
+  );
+};
+
+export default TodoItem;

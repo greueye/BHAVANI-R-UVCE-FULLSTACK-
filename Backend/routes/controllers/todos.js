6ede5ef2f2d51ed6c36aba7ce708f8c2e3a2dd0b
Bhavani R <133200979+greueye@users.noreply.github.com>
2025-05-22 16:29:49 +0530

    Create todos.js

---
+++ Backend/routes/controllers/todos.js
@@ -0,0 +1,47 @@
+const Todo = require('../models/todo');
+
+// @route   GET api/todos
+// @desc    Get all todos
+exports.getTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find().sort({ date: -1 });
+    res.json(todos);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+// @route   POST api/todos
+// @desc    Add new todo
+exports.addTodo = async (req, res) => {
+  try {
+    const newTodo = new Todo({
+      text: req.body.text,
+      completed: false
+    });
+
+    const todo = await newTodo.save();
+    res.json(todo);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+  };
+
+// @route   DELETE api/todos/:id
+// @desc    Delete todo
+exports.deleteTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+
+    if (!todo) {
+      return res.status(404).json({ msg: 'Todo not found' });
+    }
+
+    await todo.remove();
+    res.json({ msg: 'Todo removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
